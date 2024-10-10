aadd37417807c792c29b1699d280a094dc8f1c85
sahasawat <sahasawut@abbot.tech>
2024-10-10 18:04:52 +0700

    add toast component

--- src/components/ComponentProvider/HokUIProvider.tsx
+++ src/components/ComponentProvider/HokUIProvider.tsx
@@ -1,16 +1,68 @@
 import React from "react";
+import { renderToString } from "react-dom/server";
 import { I18nProvider } from "react-aria";
+import { motion } from "framer-motion";
+
+type ToastHandlerProps = {
+  children: React.ReactNode;
+  // onHover: (isHover: boolean) => void;
+  // onClick: () => void;
+  onClose: () => void;
+};
+
+type ToastHandlerState = {
+  timoutId: ReturnType<typeof setTimeout> | null;
+};
+
+class ToastHandler extends React.Component<
+  ToastHandlerProps,
+  ToastHandlerState
+> {
+  state: ToastHandlerState = {
+    timoutId: null,
+  };
+
+  constructor(props: ToastHandlerProps) {
+    super(props);
+
+    const timoutId = setTimeout(() => {
+      props.onClose();
+    }, 2500);
+
+    this.state.timoutId = timoutId;
+  }
+
+  componentWillUnmount(): void {
+    if (this.state.timoutId) {
+      clearTimeout(this.state.timoutId);
+      this.setState({ timoutId: null });
+    }
+  }
+
+  render(): React.ReactNode {
+    return (
+      <div
+      // onMouseEnter={() => props.onHover(true)}
+      // onMouseLeave={() => props.onHover(false)}
+      // onClick={props.onClick}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
+}
 
 type DialogToastRendererProps = {
   children: React.ReactNode;
 };
 
-type DialogToastRendererRef = {
-  toast: () => void;
+export type ToastOptions = {
+  render: React.ReactNode;
+  delay?: number;
 };
 
-export type ToastProps = {
-  key: string;
+type DialogToastRendererRef = {
+  toast: (options: ToastOptions) => void;
 };
 
 const dialogToastRef = React.createRef<DialogToastRendererRef>();
@@ -23,21 +75,43 @@ const DialogToastRendererRef = React.forwardRef<
   const toastContainerRef = React.useRef<HTMLDivElement | null>(null);
 
   React.useImperativeHandle(ref, () => ({
-    toast() {
-      const postNode = document.createElement("div");
-      postNode.innerHTML = "<button>This post is appended!</button>";
-      postNode.className = "bg-red-300 pointer-events-auto p-4 rounded";
-      toastContainerRef.current?.appendChild(postNode);
+    toast(options: ToastOptions) {
+      if (toastContainerRef.current) {
+        const postNode = document.createElement("div");
+        postNode.classList.add("animate-toastIn");
+        const toastElm = renderToString(
+          <div className="bg-white/40 backdrop-blur pointer-events-auto p-4 rounded shadow-sm">
+            <ToastHandler
+              onClose={() => {
+                postNode.classList.replace(
+                  "animate-toastIn",
+                  "animate-toastOut",
+                );
+                const toastTimeout = setTimeout(() => {
+                  toastContainerRef.current?.removeChild(postNode);
+                  clearTimeout(toastTimeout);
+                }, 500);
+              }}
+            >
+              {options.render}
+            </ToastHandler>
+          </div>,
+        );
+        postNode.innerHTML = toastElm;
+        toastContainerRef.current.appendChild(postNode);
+      }
     },
   }));
 
   return (
-    <div className="relative w-screen overflow-x-hidden">
-      <div
-        ref={toastContainerRef}
-        className="fixed top-4 right-4 z-50 pointer-events-none flex flex-col gap-2"
-      />
-      <div>{props.children}</div>
+    <div>
+      {props.children}
+      <motion.div className="fixed top-4 right-4 z-50 pointer-events-none">
+        <div
+          ref={toastContainerRef}
+          className="flex flex-col items-end gap-2"
+        />
+      </motion.div>
     </div>
   );
 });
@@ -59,9 +133,9 @@ function Provider(props: HokUiProviderProps) {
 
 export function useAlert() {
   return {
-    toast: () => {
+    toast: (options: ToastOptions) => {
       if (dialogToastRef.current) {
-        dialogToastRef.current.toast();
+        dialogToastRef.current.toast(options);
       }
     },
   };
