35051d588ac7b3de0644ef8238b2122ae6b7cc54
sahasawat <sahasawut@abbot.tech>
2024-11-18 10:58:18 +0700

    fix dialog props

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -4,19 +4,26 @@ import {
   DialogTrigger,
   Dialog as RACDialog,
 } from "react-aria-components";
+import { type PlacementAxis } from "react-aria";
 import { twMerge } from "tailwind-merge";
 import { Popover } from "../Popover";
 
 export interface DialogProp extends DialogProps {
   activator?: () => React.ReactNode;
   label?: string;
+  placement?: PlacementAxis;
   elementType?: React.JSXElementConstructor<any> | React.ElementType;
 }
 
 export function Dialog(props: DialogProp) {
   return (
     <DialogTrigger>
-      <Popover showArrow label={props.label} activator={props.activator}>
+      <Popover
+        showArrow
+        placement={props.placement}
+        label={props.label}
+        activator={props.activator}
+      >
         <RACDialog
           {...props}
           className={twMerge(
